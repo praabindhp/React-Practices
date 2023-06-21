f93e527dc725a973ced72d8f72bb7508b265faf1
praabindh <praabindh@codingmart.com>
2023-06-21 11:48:42 +0530

    Updated React Counter-App

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  constructor() {
+    super();
+    console.log("Constructor", this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
