9a07532af08932323ad8572939621f6717fb3558
praabindh <praabindh@codingmart.com>
2023-06-27 18:33:59 +0530

    Updated NextJS Practices

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # React-Practices
 React JavaScript Practices
 
+> Working With JSX
+> Rendering Elements
+> Conditional Rendering
+> Handling Events
+> Updating The State
+> Data Passing
+> Raise & Handle Events
+> Multiple Components In Sync
+> Functional Components
+> Lifecycle Hooks
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
-import Counter from './components/counter';
+import Counters from './components/counters';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Counter />
+  <Counters />
 );
 
 // If you want to start measuring performance in your app, pass a function
